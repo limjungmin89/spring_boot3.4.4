f13d8afdebaa8207e01989518ad4c41522cd85d6
limjungmin89 <ckzkboy@naver.com>
2025-04-09 18:20:28 +0900

    1. 자바스크립트 인라인, 인라인 each 추가

--- src/main/java/com/spring_boot/basic/BasicController.java
+++ src/main/java/com/spring_boot/basic/BasicController.java
@@ -129,9 +129,17 @@ public class BasicController {
     }
 
     @GetMapping("/block")
-    public String blokc(Model model) {
+    public String block(Model model) {
         addUsers(model);
         return "basic/block";
+    }
+
+    @GetMapping("javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+
+        return "basic/javascript";
     }
 
     private void addUsers(Model model) {
