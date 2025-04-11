c85340cf5dc440e8c9d4c9c82a6919aca8e45eb3
limjungmin89 <ckzkboy@naver.com>
2025-04-11 16:22:49 +0900

    1. validation errors Map 추가 2. 화면에 에러 발생인 경우 css, div 태그 추가

--- src/main/java/com/spring_boot/web/controller/FormItemController.java
+++ src/main/java/com/spring_boot/web/controller/FormItemController.java
@@ -8,13 +8,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * packageName    : com.spring_boot.web.controller
@@ -37,10 +35,12 @@ public class FormItemController {
 
     @ModelAttribute("regions")
     public Map<String, String> regions() {
+
         Map<String, String> regions = new LinkedHashMap<>();
         regions.put("SEOUL", "서울");
         regions.put("BUSAN", "부산");
         regions.put("JEJU", "제주");
+
         return regions;
     }
 
@@ -51,6 +51,7 @@ public class FormItemController {
 
     @ModelAttribute("deliveryCodes")
     public List<DeliveryCode> deliveryCodes() {
+
         List<DeliveryCode> deliveryCodes = new ArrayList<>();
         deliveryCodes.add(new DeliveryCode("FAST","빠른 배송"));
         deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
@@ -61,65 +62,104 @@ public class FormItemController {
 
     @GetMapping
     public String items(Model model) {
+
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
+
         return "view/form/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
+
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-
-//        Map<String, String> regions = new LinkedHashMap<>();
-//        regions.put("SEOUL", "서울");
-//        regions.put("BUSAN", "부산");
-//        regions.put("JEJU", "제주");
-//        model.addAttribute("regions", regions);
 
         return "view/form/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
-        model.addAttribute("item", new Item());
 
-//        Map<String, String> regions = new LinkedHashMap<>();
-//        regions.put("SEOUL", "서울");
-//        regions.put("BUSAN", "부산");
-//        regions.put("JEJU", "제주");
-//        model.addAttribute("regions", regions);
+        model.addAttribute("item", new Item());
 
         return "view/form/addForm";
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, Model model, RedirectAttributes redirectAttributes) {
 
-        log.info("Item >>> {}",item.getOpen() );
+        Map<String, String> errors = new HashMap<>();
+
+        if(!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재값 = " + resultPrice);
+            }
+        }
+        //검증에 실패하면 다시 입력 폼으로
+        if (!errors.isEmpty()) {
+            log.info("Here - errors {}" , errors);
+            model.addAttribute("errors", errors);
+            return "view/form/addForm";
+        }
+        log.info("Here - model {}" , model);
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
+
         return "redirect:/form/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
+
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-
-//        Map<String, String> regions = new LinkedHashMap<>();
-//        regions.put("SEOUL", "서울");
-//        regions.put("BUSAN", "부산");
-//        regions.put("JEJU", "제주");
-//        model.addAttribute("regions", regions);
 
         return "view/form/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item, Model model) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        if(StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재값 = " + resultPrice);
+            }
+        }
+        //검증에 실패하면 다시 입력 폼으로
+        if (!errors.isEmpty()) {
+            model.addAttribute("errors", errors);
+            return "view/form/editForm";
+        }
+
         itemRepository.update(itemId, item);
+
         return "redirect:/form/items/{itemId}";
     }
 
