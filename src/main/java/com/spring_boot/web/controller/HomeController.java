278df699655d946c71e158a8c689b6ccc42d8c95
limjungmin89 <ckzkboy@naver.com>
2025-04-14 12:36:31 +0900

    1. 로그인 쿠키 추가 2. 로그아웃 추가

--- src/main/java/com/spring_boot/web/controller/HomeController.java
+++ src/main/java/com/spring_boot/web/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.spring_boot.web.controller;
 
+import com.spring_boot.web.dao.MemberRepository;
+import com.spring_boot.web.domain.Member;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,10 +23,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+//    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+
+        if(memberId == null) {
+            return "home";
+        }
+
+        Member loginMember =memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+        model.addAttribute("member", loginMember);
+
+        return "loginHome";
     }
 }
